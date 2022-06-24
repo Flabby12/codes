0e2769b2d4dc492e1445e17bf1c28590f55dd108
Flabby12 <106718333+Flabby12@users.noreply.github.com>
2022-06-24 10:54:50 +0800

    Add files via upload

---
+++ newfile.py
@@ -0,0 +1,13 @@
+class Person:
+    name = "Adrian”								OUTPUT:
+    age = 20
+    
+    def __init__(self, personName, personAge): 		
+        self.name = personName
+        self.age = personAge
+    def __repr__(self):
+        return {'name':sellf.name, 'age':self.age}
+    def __str__(self):
+        return 'personName(name='+self.name+'age='+str(self.age)+')'
+
+
